f8dbd119f7655409dfc312d87e2e24cebdafd5d6
Charlie Birks <charlie@daft.games>
2025-01-18 16:22:41 +0000

    Add storage vis stub

---
+++ storage-vis/storage-vis.cpp
@@ -0,0 +1,15 @@
+#include "32blit.hpp"
+
+#include "engine/api_private.hpp"
+
+void init() {
+  blit::set_screen_mode(blit::ScreenMode::hires);
+}
+
+void render(uint32_t time_ms) {
+
+}
+
+void update(uint32_t time_ms) {
+
+}
